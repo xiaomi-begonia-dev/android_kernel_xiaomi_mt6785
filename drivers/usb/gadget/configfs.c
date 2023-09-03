Noob-214/Lightning-Kernel

usb: gadget: Silence uevent log
[  271.047828] android_work: did not send uevent (0 0 0000000000000000)

Signed-off-by: siimsek <malisimsek17@gmail.com>

drivers/usb/gadget/configfs.c
@@ -1480,7 +1480,7 @@ static void android_work(struct work_struct *data)
 	}
 
 	if (!uevent_sent) {
-		pr_info("%s: did not send uevent (%d %d %pK)\n", __func__,
+		pr_debug("%s: did not send uevent (%d %d %pK)\n", __func__,
 			gi->connected, gi->sw_connected, cdev->config);
 	}
 }

xiaomi-begonia-dev/android_kernel_xiaomi_mt6785

Signed-off-by: Charsi-Tejas <tejas@m.ms.evolution-x.org>
Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

drivers/usb/gadget/configfs.c
@@ -1559,7 +1559,7 @@ static void android_work(struct work_struct *data)
 	}
 
 	if (!uevent_sent) {
-		pr_info("%s: did not send uevent (%d %d %p)\n", __func__,
+		pr_debug("%s: did not send uevent (%d %d %p)\n", __func__,
 			gi->connected, gi->sw_connected, cdev->config);
 	}
 }